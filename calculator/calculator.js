549c75bbbc72be8359263f22cfcf754c00175ab8
Michael Houmann <mhoumann87@gmail.com>
2019-02-10 15:01:49 +0100

    Add calculator with styles

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,29 +1,16 @@
 /** 
-My calculator should contain a clickable element containing 
-an = (equal sign) with a corresponding id="equals".
+
 */
+
 /**
- * My calculator should contain 10 clickable elements containing one number
- * each from 0-9, with the following corresponding IDs: id="zero", id="one",
- * id="two", id="three", id="four", id="five", id="six", id="seven", id="eight",
- * and id="nine".
+
+/**
+ *
  */
 /**
- *  My calculator should contain 4 clickable elements each containing one of the
- * 4 primary mathematical operators with the following corresponding
- * IDs: id="add", id="subtract", id="multiply", id="divide".
+ *
  */
-/**
- * My calculator should contain a clickable element containing
- * a . (decimal point) symbol with a corresponding id="decimal".
- */
-/**
- *  My calculator should contain a clickable element with an id="clear".
- */
-/**
- * My calculator should contain an element to display values
- * with a corresponding id="display".
- */
+
 /**
  * At any time, pressing the clear button clears the input and output values,
  * and returns the calculator to its initialized state; 0 should be shown in
@@ -74,7 +61,7 @@ const Header = () => (
 
 const Footer = () => (
   <footer>
-    <div class="copy">
+    <div className="copy">
       <p>
         &copy;2019 Made by&nbsp;
         <a href="https://github.com/mhoumann87" target="_blank">
@@ -82,15 +69,15 @@ const Footer = () => (
         </a>
       </p>
     </div>
-    <div class="social">
+    <div className="social">
       <a href="https://twitter.com/MichaelHoumann" target="_blank">
-        <i class="fab fa-twitter" />
+        <i className="fab fa-twitter" />
       </a>
       <a href="https://github.com/mhoumann87" target="_blank">
-        <i class="fab fa-github" />
+        <i className="fab fa-github" />
       </a>
       <a href="www.linkedin.com/in/michael-houmann-3b03654b" target="_blank">
-        <i class="fab fa-linkedin" />
+        <i className="fab fa-linkedin" />
       </a>
     </div>
   </footer>
@@ -104,7 +91,85 @@ class Calculator extends React.Component {
   render() {
     return (
       <main>
-        <p>body</p>
+        <div className="calc-body">
+          {/*  My calculator should contain an element to display values
+             with a corresponding id="display". */}
+          <div className="display" id="display">
+            0123456789
+          </div>
+          {/* My calculator should contain 10 clickable elements containing
+          one number * each from 0-9, with the following corresponding IDs:
+          id="zero", id="one", * id="two", id="three", id="four", id="five",
+          id="six", id="seven", id="eight", * and id="nine".
+          My calculator should contain a clickable element containing 
+          an = (equal sign) with a corresponding id="equals".
+          My calculator should contain 4 clickable elements each containing one of the
+          4 primary mathematical operators with the following corresponding
+          IDs: id="add", id="subtract", id="multiply", id="divide".
+          My calculator should contain a clickable element with an id="clear".
+          My calculator should contain a clickable element containing
+          a . (decimal point) symbol with a corresponding id="decimal".
+          */}
+          <div className="keyboard">
+            <div className="btn func" id="clear">
+              C
+            </div>
+            <div className="btn" id="clear-screen">
+              CE
+            </div>
+            <div className="btn" id="reverse">
+              +/-
+            </div>
+            <div className="btn func" id="divide">
+              <i className="fas fa-divide" />
+            </div>
+            <div className="btn" id="one">
+              1
+            </div>
+            <div className="btn" id="two">
+              2
+            </div>
+            <div className="btn" id="three">
+              3
+            </div>
+            <div className="btn func" id="multiply">
+              <i className="fas fa-times" />
+            </div>
+            <div className="btn" id="four">
+              4
+            </div>
+            <div className="btn" id="five">
+              5
+            </div>
+            <div className="btn" id="six">
+              6
+            </div>
+            <div className="btn func" id="subtract">
+              <i className="fas fa-minus" />
+            </div>
+            <div className="btn" id="seven">
+              7
+            </div>
+            <div className="btn" id="eight">
+              8
+            </div>
+            <div className="btn" id="nine">
+              9
+            </div>
+            <div className="btn func" id="add">
+              <i className="fas fa-plus" />
+            </div>
+            <div className="btn" id="zero">
+              0
+            </div>
+            <div className="btn dot" id="decimal">
+              <i className="fas fa-circle" />
+            </div>
+            <div className="btn func equals" id="equals">
+              <i className="fas fa-equals" />
+            </div>
+          </div>
+        </div>
       </main>
     );
   }
