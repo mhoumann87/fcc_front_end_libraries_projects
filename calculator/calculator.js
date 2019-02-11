22a5a2c733289ad1331cf5c0cd4ac981d4a49efb
Michael Houmann <mhoumann87@gmail.com>
2019-02-11 15:13:09 +0100

    Clean up before mergin

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,45 +1,3 @@
-/**
- * At any time, pressing the clear button clears the input and output values,
- * and returns the calculator to its initialized state; 0 should be shown in
- * the element with the id of display.
- */
-/**
- * As I input numbers, I should be able to see my input in the
- * element with the id of display.
- */
-/**
- * In any order, I should be able to add, subtract, multiply and divide
- * a chain of numbers of any length, and when I hit =, the correct
- * result should be shown in the element with the id of display.
- */
-/**
- * When inputting numbers, my calculator should not allow
- * a number to begin with multiple zeros.
- */
-/**
- * When the decimal element is clicked, a . should append to the currently
- * displayed value; two . in one number should not be accepted.
- */
-/**
- * I should be able to perform any
- * operation (+, -, *, /) on numbers containing decimal points.
- */
-/**
- * If 2 or more operators are entered consecutively,
- * the operation performed should be the last operator entered.
- */
-/**
- * Pressing an operator immediately following = should start a new
- * calculation that operates on the result of the previous evaluation.
- */
-/**
- * My calculator should have several decimal places of precision when it
- * comes to rounding (note that there is no exact standard, but you should be
- *  able to handle calculations like 2 / 7 with reasonable precision to at
- * least 4 decimal places).
- */
-/** */
-
 const Header = () => (
   <header>
     <h1>React Calculator</h1>
@@ -98,19 +56,7 @@ class Calculator extends React.Component {
           <div className="display" id="display">
             {this.state.inputValue}
           </div>
-          {/* My calculator should contain 10 clickable elements containing
-          one number * each from 0-9, with the following corresponding IDs:
-          id="zero", id="one", * id="two", id="three", id="four", id="five",
-          id="six", id="seven", id="eight", * and id="nine".
-          My calculator should contain a clickable element containing 
-          an = (equal sign) with a corresponding id="equals".
-          My calculator should contain 4 clickable elements each containing one of the
-          4 primary mathematical operators with the following corresponding
-          IDs: id="add", id="subtract", id="multiply", id="divide".
-          My calculator should contain a clickable element with an id="clear".
-          My calculator should contain a clickable element containing
-          a . (decimal point) symbol with a corresponding id="decimal".
-          */}
+
           <div className="keyboard">
             <div className="btn func" id="clear">
               C
