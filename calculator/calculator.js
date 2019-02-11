aea6cb464760e0ad2f8394685eabdd1a7b4c8bd7
Michael Houmann <mhoumann87@gmail.com>
2019-02-11 15:07:06 +0100

    Add calculator component to js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,57 +1,8 @@
-/** 
-
-*/
-
 /**
-
-/**
- *
+ * This calculator in react are made for the front end libraries certicicate on
+ * www.freecodecamp.org.
+ * Made by Michael Houmann 2019
  */
-/**
- *
- */
-
-/**
- * At any time, pressing the clear button clears the input and output values,
- * and returns the calculator to its initialized state; 0 should be shown in
- * the element with the id of display.
- */
-/**
- * As I input numbers, I should be able to see my input in the
- * element with the id of display.
- */
-/**
- * In any order, I should be able to add, subtract, multiply and divide
- * a chain of numbers of any length, and when I hit =, the correct
- * result should be shown in the element with the id of display.
- */
-/**
- * When inputting numbers, my calculator should not allow
- * a number to begin with multiple zeros.
- */
-/**
- * When the decimal element is clicked, a . should append to the currently
- * displayed value; two . in one number should not be accepted.
- */
-/**
- * I should be able to perform any
- * operation (+, -, *, /) on numbers containing decimal points.
- */
-/**
- * If 2 or more operators are entered consecutively,
- * the operation performed should be the last operator entered.
- */
-/**
- * Pressing an operator immediately following = should start a new
- * calculation that operates on the result of the previous evaluation.
- */
-/**
- * My calculator should have several decimal places of precision when it
- * comes to rounding (note that there is no exact standard, but you should be
- *  able to handle calculations like 2 / 7 with reasonable precision to at
- * least 4 decimal places).
- */
-/** */
 
 const Header = () => (
   <header>
@@ -85,7 +36,145 @@ const Footer = () => (
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { operations: [] };
+    this.state = {
+      operations: [],
+      showInput: "0",
+      subTotal: 0,
+      prevOperator: "",
+      digit: false
+    };
+
+    this.clear = this.clear.bind(this);
+    this.clearScreen = this.clearScreen.bind(this);
+    this.reverse = this.reverse.bind(this);
+    this.setDigit = this.setDigit.bind(this);
+    this.handleNumbers = this.handleNumbers.bind(this);
+    this.handleOperators = this.handleOperators.bind(this);
+  }
+
+  clear() {
+    this.setState({
+      showInput: "0",
+      subTotal: 0,
+      prevOperator: "",
+      digit: false
+    });
+  }
+  clearScreen() {
+    this.setState({
+      showInput: "0"
+    });
+  }
+  reverse() {
+    let reverseNum = parseFloat(this.state.showInput) * -1;
+    this.setState({
+      showInput: reverseNum.toString()
+    });
+  }
+  setDigit() {
+    console.log("digit");
+    if (this.state.digit) {
+      return;
+    } else {
+      this.setState({
+        digit: true,
+        showInput: this.state.showInput + "."
+      });
+    }
+  }
+  handleNumbers(e) {
+    e.persist();
+    if (this.state.showInput === "0") {
+      this.state.showInput = "";
+      this.setState({
+        showInput: this.state.showInput + e.target.attributes[2].value
+      });
+    } else if (parseFloat(this.state.showInput) === this.state.subTotal) {
+      this.state.showInput = "";
+      this.setState({
+        showInput: this.state.showInput + e.target.attributes[2].value
+      });
+    } else {
+      this.setState({
+        showInput: this.state.showInput + e.target.attributes[2].value
+      });
+    }
+  }
+  handleOperators(e) {
+    e.persist();
+    let clicked = "";
+    if (e.target.classList[0] === "fas") {
+      clicked = e.target.parentElement.attributes[2].value;
+    } else {
+      clicked = e.target.attributes[2].value;
+    }
+    if (this.state.showInput === "0" || this.state.showInput === "") {
+      return;
+    } else if (this.state.prevOperator === "" && this.state.subTotal == 0) {
+      this.setState({
+        subTotal: parseFloat(this.state.showInput),
+        prevOperator: clicked
+      });
+    } else if (this.state.prevOperator === "" && this.state.subTotal !== 0) {
+      this.setState({
+        showInput: "error"
+      });
+    } else if (this.state.prevOperator === "=") {
+      this.setState({
+        prevOperator: clicked
+      });
+    } else {
+      this.operate(parseFloat(this.state.showInput), this.state.prevOperator);
+      this.setState({
+        prevOperator: clicked
+      });
+    }
+  }
+  operate(value, opr) {
+    switch (opr) {
+      case "+":
+        let addValue = this.state.subTotal + value;
+        this.setState({
+          subTotal: addValue,
+          showInput: addValue.toString()
+        });
+        break;
+      case "-":
+        let subValue = this.state.subTotal - value;
+        this.setState({
+          subTotal: subValue,
+          showInput: subValue.toString()
+        });
+        break;
+      case "*":
+        let mulValue = this.state.subTotal * value;
+        this.setState({
+          subTotal: mulValue,
+          showInput: subValue.toString()
+        });
+        break;
+      case "/":
+        if (this.state.subTotal === 0 || value === 0) {
+          this.setState({
+            showInput: "error"
+          });
+          break;
+        }
+        let divValue = this.state.subTotal / value;
+        this.setState({
+          subTotal: divValue,
+          showInput: divValue.toString()
+        });
+        break;
+      case "=":
+        let res = this.state.subTotal;
+        this.setState({
+          showInput: res,
+          subTotal: res,
+          prevOperator: "="
+        });
+        break;
+    }
   }
 
   render() {
@@ -95,82 +184,129 @@ class Calculator extends React.Component {
           {/*  My calculator should contain an element to display values
              with a corresponding id="display". */}
           <div className="display" id="display">
-            0123456789
+            {this.state.showInput}
           </div>
-          {/* My calculator should contain 10 clickable elements containing
-          one number * each from 0-9, with the following corresponding IDs:
-          id="zero", id="one", * id="two", id="three", id="four", id="five",
-          id="six", id="seven", id="eight", * and id="nine".
-          My calculator should contain a clickable element containing 
-          an = (equal sign) with a corresponding id="equals".
-          My calculator should contain 4 clickable elements each containing one of the
-          4 primary mathematical operators with the following corresponding
-          IDs: id="add", id="subtract", id="multiply", id="divide".
-          My calculator should contain a clickable element with an id="clear".
-          My calculator should contain a clickable element containing
-          a . (decimal point) symbol with a corresponding id="decimal".
-          */}
-          <div className="keyboard">
-            <div className="btn func" id="clear">
-              C
-            </div>
-            <div className="btn" id="clear-screen">
-              CE
-            </div>
-            <div className="btn" id="reverse">
-              +/-
-            </div>
-            <div className="btn func" id="divide">
-              <i className="fas fa-divide" />
-            </div>
-            <div className="btn" id="one">
-              1
-            </div>
-            <div className="btn" id="two">
-              2
-            </div>
-            <div className="btn" id="three">
-              3
-            </div>
-            <div className="btn func" id="multiply">
-              <i className="fas fa-times" />
-            </div>
-            <div className="btn" id="four">
-              4
-            </div>
-            <div className="btn" id="five">
-              5
-            </div>
-            <div className="btn" id="six">
-              6
-            </div>
-            <div className="btn func" id="subtract">
-              <i className="fas fa-minus" />
-            </div>
-            <div className="btn" id="seven">
-              7
-            </div>
-            <div className="btn" id="eight">
-              8
-            </div>
-            <div className="btn" id="nine">
-              9
-            </div>
-            <div className="btn func" id="add">
-              <i className="fas fa-plus" />
-            </div>
-            <div className="btn" id="zero">
-              0
-            </div>
-            <div className="btn dot" id="decimal">
-              <i className="fas fa-circle" />
-            </div>
-            <div className="btn func equals" id="equals">
-              <i className="fas fa-equals" />
-            </div>
-          </div>
+          <Keyboard
+            clear={this.clear}
+            clearScreen={this.clearScreen}
+            reverse={this.reverse}
+            digit={this.setDigit}
+            numbers={this.handleNumbers}
+            operator={this.handleOperators}
+          />
         </div>
       </main>
+    );
+  }
+}
+
+class Keyboard extends React.Component {
+  render() {
+    return (
+      <div className="keyboard">
+        <div
+          className="btn func"
+          id="clear"
+          value="C"
+          onClick={this.props.clear}
+        >
+          C
+        </div>
+        <div
+          className="btn"
+          id="clear-screen"
+          value="CE"
+          onClick={this.props.clearScreen}
+        >
+          CE
+        </div>
+        <div
+          className="btn"
+          id="reverse"
+          value="+/-"
+          onClick={this.props.reverse}
+        >
+          +/-
+        </div>
+        <div
+          className="btn func"
+          id="divide"
+          value="/"
+          onClick={this.props.operator}
+        >
+          <i className="fas fa-divide" />
+        </div>
+        <div className="btn" id="one" value="1" onClick={this.props.numbers}>
+          1
+        </div>
+        <div className="btn" id="two" value="2" onClick={this.props.numbers}>
+          2
+        </div>
+        <div className="btn" id="three" value="3" onClick={this.props.numbers}>
+          3
+        </div>
+        <div
+          className="btn func"
+          id="multiply"
+          value="*"
+          onClick={this.props.operator}
+        >
+          <i className="fas fa-times" />
+        </div>
+        <div className="btn" id="four" value="4" onClick={this.props.numbers}>
+          4
+        </div>
+        <div className="btn" id="five" value="5" onClick={this.props.numbers}>
+          5
+        </div>
+        <div className="btn" id="six" value="6" onClick={this.props.numbers}>
+          6
+        </div>
+        <div
+          className="btn func"
+          id="subtract"
+          value="-"
+          onClick={this.props.operator}
+        >
+          <i className="fas fa-minus" />
+        </div>
+        <div className="btn" id="seven" value="7" onClick={this.props.numbers}>
+          7
+        </div>
+        <div className="btn" id="eight" value="8" onClick={this.props.numbers}>
+          8
+        </div>
+        <div className="btn" id="nine" value="9" onClick={this.props.numbers}>
+          9
+        </div>
+        <div
+          className="btn func"
+          id="add"
+          value="+"
+          onClick={this.props.operator}
+        >
+          <i className="fas fa-plus" />
+        </div>
+        <div className="btn" id="zero" value="0" onClick={this.props.numbers}>
+          0
+        </div>
+        <div
+          className="btn dot"
+          id="decimal"
+          value="."
+          onClick={this.props.digit}
+        >
+          <i className="fas fa-circle" />
+        </div>
+        <div
+          className="btn func equals"
+          id="equals"
+          value="="
+          onClick={this.props.operator}
+        >
+          <i className="fas fa-equals" />
+        </div>
+      </div>
     );
   }
 }
